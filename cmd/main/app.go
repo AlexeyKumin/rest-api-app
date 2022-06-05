bbe373bfae98495a82c3be1b51b8d31976cefce9
Alexey Kumin <akumin12345@gmail.com>
2022-06-05 22:47:01 +0300

    simple app, logger, config

---
+++ cmd/main/app.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"first-rest/internal/user"
+	"first-rest/pkg/logging"
+	"github.com/julienschmidt/httprouter"
+	"net"
+	"net/http"
+	"time"
+)
+
+func main() {
+	logger := logging.GetLogger()
+	logger.Info("create router")
+	router := httprouter.New()
+
+	logger.Info("register user handler")
+	handler := user.NewHandler(logger)
+	handler.Register(router)
+
+	start(router)
+}
+
+func start(router *httprouter.Router) {
+	logger := logging.GetLogger()
+	logger.Info("start application")
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	logger.Info("server is listening 0.0.0.0:1234")
+	logger.Fatal(server.Serve(listener))
+}
---
+++ internal/user/handler.go
@@ -0,0 +1,60 @@
+package user
+
+import (
+	"first-rest/internal/handlers"
+	"first-rest/pkg/logging"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+var _ handlers.Handler = &handler{}
+
+const (
+	usersURL = "/users"
+	userURL  = "/users/:uuid"
+)
+
+type handler struct {
+	logger logging.Logger
+}
+
+func NewHandler(logger logging.Logger) handlers.Handler {
+	return &handler{
+		logger: logger,
+	}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersURL, h.GetList)
+	router.GET(userURL, h.GetUserByUUID)
+	router.POST(usersURL, h.CreateUser)
+	router.PUT(userURL, h.UpdateUser)
+	router.PATCH(userURL, h.PartiallyUpdateUser)
+	router.DELETE(userURL, h.DeleteUser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("list of users"))
+}
+
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("get users"))
+}
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
+	w.Write([]byte("create user"))
+}
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("update user"))
+}
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("partial update user"))
+}
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("delete user"))
+}
